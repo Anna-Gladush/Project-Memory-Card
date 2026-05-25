4bca62f12d8f2112e957ccace2c5b7499dde0f0c
Anna-Gladush <ann.gladush@gmail.com>
2026-05-25 17:50:13 +0200

    Fixed fetch, now browser doesnt give a CORS policy error + fetches multiple results + result is shown on the page as a Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,54 @@
-// import { useEffect, useState } from 'react'
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react'
 import Card from './components/card'
+import Spinner from "./components/spinner"
+
 // Scoreboard - current score, and a “Best Score”
 // There should be a function that displays the cards in a random order anytime a user clicks one. Be sure to invoke that function when the component mounts.
 
-//  8 or 12 cards
+// const API_KEY = import.meta.env.API_KEY;
+// const API_BASE_URL = `http://www.comicvine.com/api/`
 
-const API_KEY = import.meta.env.API_KEY;
-const API_BASE_URL = `http://www.comicvine.com/api/`
-
-// <resource>/?api_key=<api_key>&filter=<filter>:<value>&format=json
-
-const characters = ['Daredevil', 'Cyclops', 'Human Nightcrawler', 'Gambit', 'Iron Fist', 'Hawkeye', 'Emma Frost', 'Storm', 'Jubilee', 'Jessica Jones', 'Kate Bishop', 'Linda Carter']
 function App() {
-  // const [characterData, setCharacterData] = useState({})
+  
+  let chara = []
+  const [characterData, setCharacterData] = useState(chara)
+  const [isLoading, setIsLoading] = useState(false)
+  const characters = ['Daredevil', 'Cyclops', 'Nightcrawler', 'Gambit', 'Iron Fist', 'Hawkeye', 'Emma Frost', 'Storm', 'Jubilee', 'Jessica Jones', 'Kate Bishop', 'Linda Carter']
 
-  const getCharacterData = async (char_name) => {
-    const params = new URLSearchParams({
-      api_key: API_KEY,
-      field_list: 'name,image',
-      limit: 12,
-      filter: `name:${char_name}`,
-      format: 'json'
-    })
 
-    try {
-      const response = await fetch(`${API_BASE_URL}/?${params}`);
 
-      if (!response.ok) {
-        throw new Error(`Failed fetching data. Response status: ${response.status}`);
-      }
-      const data = await response.json();
-      return data
-      // setCharacterData(data || {})
-    } catch(err) {
-      console.log(err)
-    }
-  }
-	
+// eslint-disable-next-line react-hooks/set-state-in-effect
+useEffect(() => {
+  	const getCharacterData = async () => {
+    setIsLoading(true);
+    const promises = characters.map((char) => 
+      fetch(`https://comicvine.gamespot.com/api/characters/?api_key=6b255a47b25c88227c57121c75356c59ae586b71&field_list=name,image,id&limit=1&filter=name:${char}&format=json`).then((res) => res.json()).then((data) => ({
+            data: data.results[0]
+          })))
+      const results = await Promise.all(promises);
+      setCharacterData(results)
+      console.log(results)
+      setIsLoading(false);
+      };
+})
 
-  // http://www.comicvine.com/api/characters/?api_key=6b255a47b25c88227c57121c75356c59ae586b71&field_list=name,image&limit=1&filter=name:Daredevil,gender:male&format=json
   return (
     <>
-      {/* {characters.map(async char => {
-        const ch_info = await getCharacterData(char)
-        return (
-          <li>{ch_info.name}</li>
-        )
-      })} */}
-      <Card img='https://comicvine.gamespot.com/a/uploads/scale_small/11118/111187046/7397359-0398898002-EQH1ysWWsAA7QLf' name='Daredevil'/>
+    <header>
+      {/* SCOREBOARD */}
+    </header>
+    <main>
+      {isLoading ? ( <Spinner /> ) :
+      (<div className='memory-card-deck'>
+        {characterData.map(char => {
+          return (
+            <Card img={char.data.image.original_url} name={char.data.name} key={char.data.id}/>
+          )
+        })}
+      </div>)
+      }
+    </main>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://www.comicvine.com/api/',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 })
